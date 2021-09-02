48d536225000f7683bb977d8ca9ec214edc7a3d1
Anahi Rendon espinosa <arendon-@1-C-1.42heilbronn.de>
2021-09-02 20:13:59 +0200

    funcion strnstr

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: arendon- <arendon-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/20 16:19:59 by arendon-          #+#    #+#             */
-/*   Updated: 2021/09/02 15:12:14 by arendon-         ###   ########.fr       */
+/*   Updated: 2021/09/02 15:38:32 by arendon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,5 +35,6 @@ char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 
 #endif
