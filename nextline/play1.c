d4fe70c4d2d16023081479eb325197c983aacc13
Anahi Rendon espinosa <arendon-@1-C-13.42heilbronn.de>
2021-11-12 19:19:07 +0100

    updated

---
+++ nextline/play1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	FILE *fPointer;
+	fPointer = fopen("text.txt", "r");
+	char singleLine[150];
+
+	while(!feof(fPointer))
+	{
+		fgets(singleLine, 150, fPointer);
+		puts(singleLine);
+	}
+	fclose(fPointer);
+
+	return 0;
+}
